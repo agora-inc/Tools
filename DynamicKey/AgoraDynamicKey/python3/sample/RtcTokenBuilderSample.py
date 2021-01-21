f5333a684c9128a6f2761b30d878dbea77eea627
remymess <remy.messadene@gmail.com>
2021-01-21 16:14:36 +0100

    Adding agora.stream info for debug

--- DynamicKey/AgoraDynamicKey/python3/sample/RtcTokenBuilderSample.py
+++ DynamicKey/AgoraDynamicKey/python3/sample/RtcTokenBuilderSample.py
@@ -6,23 +6,37 @@ import os
 import time
 from random import randint
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.RtcTokenBuilder import RtcTokenBuilder,Role_Attendee
+from src.RtcTokenBuilder import RtcTokenBuilder, Role_Attendee
 
-appID = "970CA35de60c44645bbae8a215061b33"
-appCertificate = "5CFd2fd1755d40ecb72977518be15d3b"
-channelName = "7d72365eb983485397e3e3f9d460bdda"
-uid = 2882341273
+appID = "f68f8f99e18d4c76b7e03b2505f08ee3"
+appCertificate = "6fa04d10a03442f09b859f6d91bbea58"
+channelName = "xyz"
+uid = "abc-55441-u1"
 userAccount = "2882341273"
 expireTimeInSeconds = 3600
 currentTimestamp = int(time.time())
-privilegeExpiredTs = currentTimestamp + expireTimeInSeconds
-
+# privilegeExpiredTs = currentTimestamp + expireTimeInSeconds
+privilegeExpiredTs = 1611010057 + 60
+role = 1
 
 def main():
-    token = RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, Role_Attendee, privilegeExpiredTs)
+    token = RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs)
     print("Token with int uid: {}".format(token))
-    token = RtcTokenBuilder.buildTokenWithAccount(appID, appCertificate, channelName, userAccount, Role_Attendee, privilegeExpiredTs)
+    token = RtcTokenBuilder.buildTokenWithAccount(appID, appCertificate, channelName, userAccount, role, privilegeExpiredTs)
     print("Token with user account: {}".format(token))
+
+
+    appId1 = "f68f8f99e18d4c76b7e03b2505f08ee3"
+    appCertificate1 = "6fa04d10a03442f09b859f6d91bbea58"
+    channelName1 = "xyz"
+    uid1 = "abc-55441-u1"
+    role1 = str(1)
+
+    print("its us now")
+    token = RtcTokenBuilder.buildTokenWithUid(appId1, appCertificate1, channelName1, uid1, role1, privilegeExpiredTs)
+    print(token)
+
+
 
 if __name__ == "__main__":
     main()
